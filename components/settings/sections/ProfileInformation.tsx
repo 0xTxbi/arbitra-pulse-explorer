c3da5629ac352671220d6fc59d0dae5635e1064b
0xTxbi <techiejossy@gmail.com>
2023-12-24 21:21:08 +0100

    account settings: profile information

--- components/settings/sections/ProfileInformation.tsx
+++ components/settings/sections/ProfileInformation.tsx
@@ -1,11 +1,47 @@
 "use client";
 
-import { Title } from "@mantine/core";
+import {
+	Button,
+	Container,
+	Divider,
+	Group,
+	TextInput,
+	Title,
+} from "@mantine/core";
+import { IconAt, IconEdit } from "@tabler/icons-react";
 
 export function ProfileInformation() {
 	return (
-		<>
-			<Title>Profile Information</Title>
-		</>
+		<Container>
+			<Title mb={20}>Profile Information</Title>
+
+			{/* edit button */}
+			<Group justify="flex-end">
+				<Button
+					size="xs"
+					leftSection={<IconEdit size={12} />}
+				>
+					Edit
+				</Button>
+			</Group>
+
+			{/* input */}
+			<TextInput
+				leftSectionPointerEvents="none"
+				disabled
+				leftSection={<IconAt size={12} />}
+				label="Your Username"
+				placeholder="Your Username"
+				value="txbi"
+			/>
+			<TextInput
+				mt="md"
+				disabled
+				rightSectionPointerEvents="none"
+				label="Your email"
+				placeholder="Your email"
+				value="txbi@arbitra.pulse"
+			/>
+		</Container>
 	);
 }
