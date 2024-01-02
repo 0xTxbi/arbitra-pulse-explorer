6834a13130196943ea6cec92fb81a5cc6184bd3b
0xTxbi <techiejossy@gmail.com>
2024-01-02 04:16:44 +0100

    useWatchlist hook

---
+++ hooks/useWatchlist.ts
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+const useWatchlist = () => {
+	const [watchlistInfo, setWatchlistInfo] = useState(null);
+
+	const [watchlistLoading, setWatchlistLoading] =
+		useState<boolean>(false);
+	const [watchlistError, setWatchlistError] = useState<string | null>(
+		null
+	);
+
+	// function to fetch stock information
+	const fetchWatchlist = async () => {
+		try {
+			setWatchlistLoading(true);
+
+			const response = await fetch(
+				`https://arbitra-pulse-auth.fly.dev/watchlist`
+			);
+
+			// check if request was successful – status code 200
+			if (response.ok) {
+				const data = await response.json();
+
+				setWatchlistInfo(data);
+				// reset error state
+				setWatchlistError(null);
+			} else {
+				// handle non-successful response
+				setWatchlistError(
+					"Failed to fetch stock information"
+				);
+			}
+		} catch (error) {
+			// handle any unexpected errors during the request
+			setWatchlistError(
+				"An error occurred while fetching stock information"
+			);
+		} finally {
+			// set loading back to false after the request completion
+			setWatchlistLoading(false);
+		}
+	};
+
+	// automatically fetch stock info when the component loads
+	useEffect(() => {
+		fetchWatchlist();
+	}, []);
+
+	return { watchlistInfo, watchlistLoading, watchlistError };
+};
+
+export default useWatchlist;
