d86e396a7ae15cab887027959063aaa4e0137e2b
0xTxbi <techiejossy@gmail.com>
2024-01-03 21:24:57 +0100

    add stock to watchlist function

--- hooks/useWatchlist.ts
+++ hooks/useWatchlist.ts
@@ -5,12 +5,13 @@ const useWatchlist = () => {
 	const [cookies] = useCookies(["token"]);
 
 	const [watchlistInfo, setWatchlistInfo] = useState(null);
-
 	const [watchlistLoading, setWatchlistLoading] =
 		useState<boolean>(false);
 	const [watchlistError, setWatchlistError] = useState<string | null>(
 		null
 	);
+	const [watchlistAddSuccess, setWatchlistAddSuccess] =
+		useState<boolean>(false);
 
 	// function to fetch stock information
 	const fetchWatchlist = async () => {
@@ -50,12 +51,51 @@ const useWatchlist = () => {
 		}
 	};
 
+	// function to add stock symbol to watchlist
+	const addToWatchlist = async (stockSymbol: string) => {
+		try {
+			setWatchlistLoading(true);
+
+			const response = await fetch(
+				`https://arbitra-pulse-stock-info.fly.dev/watchlist/add/${stockSymbol}`,
+				{
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${cookies.token}`,
+					},
+				}
+			);
+
+			if (response.ok) {
+				setWatchlistAddSuccess(true);
+				// reset error state
+				setWatchlistError(null);
+			} else {
+				setWatchlistError(
+					`Failed to add ${stockSymbol} to your watchlist. Please try again`
+				);
+			}
+		} catch (error) {
+			setWatchlistError(
+				`An error occurred while adding ${stockSymbol} to watchlist`
+			);
+		} finally {
+			setWatchlistLoading(false);
+		}
+	};
+
 	// automatically fetch stock info when the component loads
 	useEffect(() => {
 		fetchWatchlist();
 	}, []);
 
-	return { watchlistInfo, watchlistLoading, watchlistError };
+	return {
+		watchlistInfo,
+		addToWatchlist,
+		watchlistAddSuccess,
+		watchlistLoading,
+		watchlistError,
+	};
 };
 
 export default useWatchlist;
