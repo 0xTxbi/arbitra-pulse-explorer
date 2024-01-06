73d4589c16daa5ae0127548c90a6ed4a88b4bdd4
0xTxbi <techiejossy@gmail.com>
2024-01-06 10:56:39 +0100

    middleware: remove unused imports

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { cookies } from "next/headers";
-import { jwtDecode } from "jwt-decode";
 import { checkJWTValidity } from "./utils/checkJwtValidity";
 
 export default async function middleware(req: NextRequest) {
